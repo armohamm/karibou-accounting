9934a5316fa314f62fc6827241c3929f17e60b32
Mark Hartman <m.hartman@karibou.nl>
2019-01-28 18:25:14 +0100

    Removed a superfluous controller by merging it with another controller

--- Accounting.Module/Controllers/FilterByYearController.cs
+++ Accounting.Module/Controllers/FilterByYearController.cs
@@ -1,13 +1,28 @@
 ﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using System;
 using System.ComponentModel;
 using System.Linq;
 
 namespace Accounting.Module.Controllers
 {
     [DesignerCategory("Code")]
-    public abstract class FilterObjectsByYearController<T> : FilterObjectsController<T> where T : ISupportDate
+    public abstract class FilterObjectsByYearController<T> : ObjectViewController<ListView, T> where T : ISupportDate
     {
+        protected FilterObjectsByYearController()
+        {
+            FilterObjectsAction = new SingleChoiceAction(this, "Filter" + typeof(T).Name, PredefinedCategory.FullTextSearch);
+            FilterObjectsAction.Caption = "Filter";
+            FilterObjectsAction.SelectedItemChanged += FilterAction_SelectedItemChanged;
+
+            RegisterActions(FilterObjectsAction);
+        }
+
+        public SingleChoiceAction FilterObjectsAction { get; }
+
         protected virtual IQueryable<T> GetObjectsQuery()
         {
             return ObjectSpace.GetObjectsQuery<T>();
@@ -17,6 +32,8 @@ namespace Accounting.Module.Controllers
         {
             base.OnActivated();
 
+            FilterObjectsAction.Items.Add(new ChoiceActionItem("All", CaptionHelper.GetLocalizedText("Texts", "All"), null));
+
             foreach (var obj in GetObjectsQuery().GroupBy(x => x.Date.Year).Select(x => x.First()))
             {
                 FilterObjectsAction.Items.Add(new ChoiceActionItem(obj.Date.Year.ToString(), obj.Date.Year.ToString(), CriteriaOperator.Parse("GetYear(Date) = ?", obj.Date.Year)));
@@ -25,6 +42,21 @@ namespace Accounting.Module.Controllers
             FilterObjectsAction.SelectedItem =
                 FilterObjectsAction.Items.FirstOrDefault(x => x.Id == Application.Model.ActionDesign.Actions[FilterObjectsAction.Id].GetValue<string>("SelectedFilter")) ??
                 FilterObjectsAction.Items.First(x => x.Id == "All");
+        }
+
+        protected override void OnDeactivated()
+        {
+            base.OnDeactivated();
+            FilterObjectsAction.Items.Clear();
+        }
+
+        private void FilterAction_SelectedItemChanged(object sender, EventArgs e)
+        {
+            if (FilterObjectsAction.SelectedItem != null)
+            {
+                View.CollectionSource.Criteria[Name] = (CriteriaOperator)FilterObjectsAction.SelectedItem.Data;
+                Application.Model.ActionDesign.Actions["Filter" + typeof(T).Name].SetValue("SelectedFilter", FilterObjectsAction.SelectedItem.Id);
+            }
         }
     }
 }
