7bd9287dc387ed6ab33b971aa6ffaa76ef51a09c
Mark Hartman <m.hartman@karibou.nl>
2019-03-02 12:08:24 +0100

    Improved the default country configuration

--- Accounting.Module/Configuration/DefaultCountryConfiguration.cs
+++ Accounting.Module/Configuration/DefaultCountryConfiguration.cs
@@ -1,13 +1,29 @@
-﻿using System.Xml.Serialization;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace Accounting.Module.Configuration
 {
-    public class DefaultCountryConfiguration
+    public class DefaultCountryConfiguration : IEnumerable<DefaultCountry>
     {
         [XmlElement("country")]
         public DefaultCountry[] Countries { get; set; }
 
         [XmlAttribute("default")]
         public string Default { get; set; }
+
+        #region IEnumerable
+
+        public IEnumerator<DefaultCountry> GetEnumerator()
+        {
+            return ((IEnumerable<DefaultCountry>)Countries).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<DefaultCountry>)Countries).GetEnumerator();
+        }
+
+        #endregion IEnumerable
     }
 }
