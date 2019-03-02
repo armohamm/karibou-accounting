2e5351b1d6fca43f14c7c49d0b11dbbca9ec8ea6
Mark Hartman <m.hartman@karibou.nl>
2019-03-02 10:56:41 +0100

    Cleaned up the controller that handles the startup navigation item

--- Accounting.Module/Controllers/ShowStartupNavigationItemController.cs
+++ Accounting.Module/Controllers/ShowStartupNavigationItemController.cs
@@ -1,6 +1,4 @@
 ﻿using Accounting.Module.BusinessObjects;
-using Accounting.Module.Configuration;
-using DevExpress.Data.Filtering;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.SystemModule;
@@ -12,8 +10,6 @@ namespace Accounting.Module.Controllers
     [DesignerCategory("Code")]
     public class ShowStartupNavigationItemController : WindowController
     {
-        private ShowNavigationItemController showNavigationItemController;
-
         public ShowStartupNavigationItemController()
         {
             TargetWindowType = WindowType.Main;
@@ -27,35 +23,20 @@ namespace Accounting.Module.Controllers
             {
                 if (objectSpace.GetObjectsCount(typeof(Company), null) == 0)
                 {
-                    this.showNavigationItemController = Frame.GetController<ShowNavigationItemController>();
-                    if (this.showNavigationItemController != null)
-                    {
-                        this.showNavigationItemController.CustomShowNavigationItem += ShowNavigationItemController_CustomShowNavigationItem;
-                    }
+                    Frame.GetController<ShowNavigationItemController>().CustomShowNavigationItem += ShowNavigationItemController_CustomShowNavigationItem;
                 }
-            }
-        }
-
-        protected override void OnDeactivated()
-        {
-            base.OnDeactivated();
-
-            if (this.showNavigationItemController != null)
-            {
-                this.showNavigationItemController.CustomShowNavigationItem -= ShowNavigationItemController_CustomShowNavigationItem;
             }
         }
 
         private void ObjectSpace_Committed(object sender, EventArgs e)
         {
             var objectSpace = (IObjectSpace)sender;
-            var showNavigationItemAction = this.showNavigationItemController.ShowNavigationItemAction;
-
-            showNavigationItemAction.SelectedItem = showNavigationItemAction.Items.FindItemByID("Settings").Items.FindItemByID("Company");
-            showNavigationItemAction.Enabled[Name] = true;
-
             objectSpace.Committed -= ObjectSpace_Committed;
             objectSpace.Disposed -= ObjectSpace_Disposed;
+
+            var showNavigationItemAction = Frame.GetController<ShowNavigationItemController>().ShowNavigationItemAction;
+            showNavigationItemAction.SelectedItem = showNavigationItemAction.Items.FindItemByID("Settings").Items.FindItemByID("Company");
+            showNavigationItemAction.Enabled[Name] = true;
         }
 
         private void ObjectSpace_Disposed(object sender, EventArgs e)
@@ -65,18 +46,16 @@ namespace Accounting.Module.Controllers
 
         private void ShowNavigationItemController_CustomShowNavigationItem(object sender, CustomShowNavigationItemEventArgs e)
         {
-            this.showNavigationItemController.CustomShowNavigationItem -= ShowNavigationItemController_CustomShowNavigationItem;
-            this.showNavigationItemController.ShowNavigationItemAction.Enabled[Name] = false;
+            var showNavigationItemController = (ShowNavigationItemController)sender;
+            showNavigationItemController.CustomShowNavigationItem -= ShowNavigationItemController_CustomShowNavigationItem;
+            showNavigationItemController.ShowNavigationItemAction.Enabled[Name] = false;
 
             var objectSpace = Application.CreateObjectSpace();
-            var company = objectSpace.CreateObject<Company>();
-            var detailView = Application.CreateDetailView(objectSpace, company, true);
-
-            company.Country = objectSpace.FindObject<Country>(new BinaryOperator("Code", DefaultConfiguration.Instance.Countries.Default));
-            detailView.ViewEditMode = ViewEditMode.Edit;
-
             objectSpace.Committed += ObjectSpace_Committed;
             objectSpace.Disposed += ObjectSpace_Disposed;
+
+            var detailView = Application.CreateDetailView(objectSpace, objectSpace.CreateObject<Company>(), true);
+            detailView.ViewEditMode = ViewEditMode.Edit;
 
             e.ActionArguments.ShowViewParameters.CreatedView = detailView;
             e.Handled = true;
